2fd07db8a3e2aa6a6e4d051a402787cba073d24f
Rafael Geronimo <rafaelgeronimofx@gmail.com>
2021-01-19 18:30:33 +0000

    Correção da numeração  da ordem das questões

---
+++ Lista 01/Ex02.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    Console.WriteLine("Informe o número da sua matrícula: ");
+    string x = Console.ReadLine();
+
+    Console.WriteLine ($"A sua matrícula é {x}");
+  }
+}
